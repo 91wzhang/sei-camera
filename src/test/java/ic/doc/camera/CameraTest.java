45378442c2bfaaf2738d3a35bb885a3f4bbe98a0
summer <summerlyudd@hotmail.com>
2015-01-22 18:36:15 +0000

    camera mockito first commit

--- src/test/java/ic/doc/camera/CameraTest.java
+++ src/test/java/ic/doc/camera/CameraTest.java
@@ -1,16 +1,74 @@
 package ic.doc.camera;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CameraTest {
 
-    @Test
-    public void switchingTheCameraOnPowersUpTheSensor() {
+	Camera camera;
+	Sensor sensor;
+	MemoryCard memoryCard;
 
-         // write your test here
-    }
+	@Before
+	public void setup() {
+		memoryCard = Mockito.mock(MemoryCard.class);
+		sensor = Mockito.mock(Sensor.class);
+		camera = new Camera(memoryCard, sensor);
+	}
+
+	@Test
+	public void switchingTheCameraOnPowersUpTheSensor() {
+		camera.powerOn();
+		Mockito.verify(sensor).powerUp();
+	}
+
+	@Test
+	public void switchingTheCameraOffPowersDownTheSensor() {
+		camera.powerOff();
+		Mockito.verify(sensor).powerDown();
+	}
+
+	@Test
+	public void perssingTheShutterWhenPowerOff() {
+		camera.powerOff();
+		camera.pressShutter();
+		Mockito.verify(sensor).powerDown();
+		Mockito.verify(sensor, Mockito.never()).readData();
+	}
+
+	@Test
+	public void perssingTheShutterWithPowerOn() {
+		camera.powerOn();
+		camera.pressShutter();
+
+		Mockito.verify(sensor).readData();
+		Mockito.verify(memoryCard).write((Mockito.any(byte[].class)));
+	}
+
+	@Test
+	public void switchingCameraOffWhenWriting() {
+		camera.powerOn();
+		camera.pressShutter();
+		camera.powerOff();
+
+		Mockito.verify(sensor, Mockito.never()).powerDown();
+	}
+
+	@Test
+	public void powerDownSensorOnceWritingCompleted() {
+		camera.powerOn();
+		camera.pressShutter();
+		camera.powerOff();
+
+		Mockito.verify(sensor, Mockito.never()).powerDown();
+
+		camera.writeComplete();
+		Mockito.verify(sensor).powerDown();
+
+	}
 
 }
