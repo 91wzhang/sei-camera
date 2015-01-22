0b7abb787c92d4403bb6b62c8816f1b74cc30093
Wei Zhang <wz2313@imperial.ac.uk>
2015-01-22 18:14:54 +0000

    Version 1.0, tests passed

--- src/main/java/ic/doc/camera/Camera.java
+++ src/main/java/ic/doc/camera/Camera.java
@@ -1,21 +1,81 @@
 package ic.doc.camera;
 
+/**
+ * Implementation of the camera class
+ * @author Dandan Lyu, Wei Zhang
+ */
 public class Camera implements WriteListener {
 
+	/**
+	 * Member
+	 * Memory card of the camera. 
+	 */
 	private final MemoryCard memoryCard;
+	
+	/**
+	 * Member
+	 * Sensor of the camera
+	 */
 	private final Sensor sensor;
-	private boolean isPowerOn;
+	
+	/**
+	 * Flag 
+	 * If the data is being written.
+	 */
 	private boolean isBusy;
-	private boolean isPowerOffRequested;
+	
+	/**
+	 * Flag 
+	 * If a sensor power down is requested.
+	 */
+	private boolean isPowerDownRequested;
+	
+	/**
+	 * Flag
+	 * If the camera is powered on.	 
+	 */
+	private boolean isPowerOn;	
 
+	/**
+	 * Constructor	 
+	 * @param memoryCard
+	 * @param sensor
+	 */
 	public Camera(MemoryCard memoryCard, Sensor sensor) {
 		this.memoryCard = memoryCard;
 		this.sensor = sensor;        
 		this.isPowerOn = false;
 		this.isBusy = false;
-		this.isPowerOffRequested = false;
+		this.isPowerDownRequested = false;
 	}
 
+	/**
+	 * Method
+	 * Power off the camera if not writing data,
+	 * else request a power down of the sensor.
+	 */
+	public void powerOff() {
+		if (!this.isBusy) {
+			this.sensor.powerDown();
+		} else {
+			this.isPowerDownRequested = true;
+		}		
+	}
+
+	/**
+	 * Method
+	 * Power on the camera; power up the sensor.
+	 */
+	public void powerOn() {
+		this.sensor.powerUp();
+		this.isPowerOn = true;
+	}
+
+	/**
+	 * Method
+	 * If camera is on copy data from the sensor 
+	 * to the memory card, else do nothing.
+	 */
 	public void pressShutter() {
 		if (this.isPowerOn) {
 			this.isBusy = true;
@@ -23,26 +83,18 @@ public class Camera implements WriteListener {
 		} 
 	}
 
-	public void powerOn() {
-		this.sensor.powerUp();
-		this.isPowerOn = true;
-	}
-
-	public void powerOff() {
-		if (!this.isBusy) {
-			this.sensor.powerDown();
-		} else {
-			this.isPowerOffRequested = true;
-		}
-		
-	}
-
+	/**
+	 * Method
+	 * Event handler when write complete is signaled.
+	 * Set flag isBusy to false;
+	 * If a power down is requested, power down the sensor.
+	 */
 	@Override
 	public void writeComplete() {
 		this.isBusy = false;
-		if (this.isPowerOffRequested) {
+		if (this.isPowerDownRequested) {
 			this.sensor.powerDown();
-			this.isPowerOffRequested = false;
+			this.isPowerDownRequested = false;
 		}
 	}   
 }
--- src/test/java/ic/doc/camera/CameraTest.java
+++ src/test/java/ic/doc/camera/CameraTest.java
@@ -1,102 +1,180 @@
 package ic.doc.camera;
 
 import org.jmock.Expectations;
+import org.jmock.Sequence;
 import org.jmock.integration.junit4.JUnitRuleMockery;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+/**
+ * Unit tests for class Camera
+ */
 public class CameraTest {
 
+	/**
+	 * Test object
+	 */
 	Camera camera;
-	Sensor sensor;
+	
+	/**
+	 * Mock objects
+	 */
+	Sensor sensor;		
 	MemoryCard memoryCard;
 	
+	/**
+	 * Test context
+	 */
     @Rule
     public JUnitRuleMockery context = new JUnitRuleMockery();
     
+    /**
+     * Sequence of invocations
+     */
+    Sequence testSequence;
+    
+    /**
+     * Test initialization before every test cases 
+     */
     @Before
     public void setup(){
     	memoryCard  = context.mock(MemoryCard.class);
     	sensor  = context.mock(Sensor.class);                
         camera = new Camera(memoryCard, sensor);
+        testSequence = context.sequence("testSequence");
     }
 
+    /**
+     * Test Case 
+     * Switching the camera on powers up the sensor.
+     */
     @Test
     public void switchingTheCameraOnPowersUpTheSensor() {                        
-         context.checking(new Expectations() {{
-        	 exactly(1).of(sensor).powerUp();
-         }});
-         
+         switchOn();         
          camera.powerOn();
     }
     
+    /**
+     * Test Case
+     * Switching the camera off powers down the sensor
+     */
     @Test
-    public void switchingTheCameraOffPowersDownTheSensor() {        
-        context.checking(new Expectations() {{
-        	exactly(1).of(sensor).powerDown();
-        }});
-        
+    public void switchingTheCameraOffPowersDownTheSensor() {    	
+        switchOff();        
         camera.powerOff();
     }
     
+    /**
+     * Test Case
+     * Pressing the shutter when the power is off does nothing
+     */
     @Test
-    public void perssingTheShutterWhenPowerOff() {    	                                       
+    public void perssingTheShutterWhenPowerOff() {
+    	switchOff();
+        camera.powerOff();        
+
         context.checking(new Expectations() {{
-        	exactly(1).of(sensor).powerDown();
-        	never(sensor).readData();
-        	never(memoryCard).write(with(any(byte[].class)));
+        	never(sensor);
+        	never(memoryCard);
         }});        
+        
+        camera.pressShutter();
+    }
+    
+    /**
+     * Test Case
+     * Pressing the shutter when the power is on copies data from
+     * the sensor to the memory card.
+     */
+    @Test
+    public void perssingTheShutterWithPowerOn() {
+    	switchOn();
+        camera.powerOn();
+
+    	writingData();               
+        camera.pressShutter();
+    }
+    
+    /**
+     * Test Case
+     * If data is currently being written, switching the camera 
+     * off does not power down the sensor.
+     */
+    @Test
+    public void switchingCameraOffWhenWriting() {    	                                                     
+        switchOn();        
+        camera.powerOn();
+        
+        writingData();
+        camera.pressShutter();        
+        
+        context.checking(new Expectations() {{
+        	never(sensor).powerDown();
+        	inSequence(testSequence);
+        }});
         
         camera.powerOff();        
-        camera.pressShutter();
-        context.assertIsSatisfied();
     }
     
-    @Test
-    public void perssingTheShutterWithPowerOn() {    	   
-        context.checking(new Expectations() {{
-        	exactly(1).of(sensor).powerUp();
-        	exactly(1).of(sensor).readData();
-        	exactly(1).of(memoryCard).write(with(any(byte[].class)));
-        }});
-        
-        camera.powerOn();
-        camera.pressShutter();
-    }
-    
-    @Test
-    public void switchingCameraOffWhenWriting() {    	                             
-        context.checking(new Expectations() {{
-        	exactly(1).of(sensor).powerUp();
-        	exactly(1).of(sensor).readData();
-        	exactly(1).of(memoryCard).write(with(any(byte[].class)));
-        	never(sensor).powerDown();
-        }});        
-        
-        camera.powerOn();
-        camera.pressShutter();
-        camera.powerOff();
-    }
-    
+    /**
+     * Test Case
+     * Once writing the data has completed, then the camera 
+     * powers down the sensor.
+     */
     @Test
     public void powerDownSensorOnceWritingCompleted() {    	        
-        context.checking(new Expectations() {{
-        	exactly(1).of(sensor).powerUp();
-        	exactly(1).of(sensor).readData();
-        	exactly(1).of(memoryCard).write(with(any(byte[].class)));
-        	never(sensor).powerDown();
-        }});
-        
+    	switchOn();        
         camera.powerOn();
-        camera.pressShutter();
-        camera.powerOff();                
-                        
+        
+        writingData();
+        camera.pressShutter();        
+        
         context.checking(new Expectations() {{
-        	exactly(1).of(sensor).powerDown();
+        	never(sensor).powerDown();  
+        	inSequence(testSequence);
         }});
         
+        camera.powerOff(); 
+        
+        switchOff();        
         camera.writeComplete();
     }
-
+    
+    
+    /**
+     * Helper 
+     * Validate the sensor is powered up.
+     */    
+    private void switchOn(){
+    	context.checking(new Expectations() {{
+    		exactly(1).of(sensor).powerUp();
+    		inSequence(testSequence);
+        }});
+    }
+    
+    /**
+     * Helper
+     * Validate the sensor is powered down.
+     */
+    private void switchOff(){
+    	context.checking(new Expectations() {{
+        	exactly(1).of(sensor).powerDown();
+        	inSequence(testSequence);
+        }});
+    }
+    
+    /**
+     * Helper 
+     * Validate the data is being copied from the sensor 
+     * to the memory card.
+     */
+    private void writingData(){
+    	context.checking(new Expectations() {{
+    		exactly(1).of(sensor).readData();
+        	inSequence(testSequence);
+        	exactly(1).of(memoryCard).write(with(any(byte[].class))); 
+        	inSequence(testSequence);        	
+        }});
+    }
 }
