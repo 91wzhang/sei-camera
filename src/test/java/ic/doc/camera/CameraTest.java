a7dae907ea488e8bca6ff92be5a41b527b847bd2
summer <summerlyudd@hotmail.com>
2015-01-22 18:51:31 +0000

    Camera_Mockito Second commit

--- src/test/java/ic/doc/camera/CameraTest.java
+++ src/test/java/ic/doc/camera/CameraTest.java
@@ -6,13 +6,26 @@ import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
+/**
+ * Unit tests for class Camera
+ */
 @RunWith(MockitoJUnitRunner.class)
 public class CameraTest {
 
+	/**
+	 * Test object
+	 */
 	Camera camera;
+	
+	/**
+	 * Mock objects
+	 */
 	Sensor sensor;
 	MemoryCard memoryCard;
 
+	/**
+     * Test initialization before every test cases 
+     */
 	@Before
 	public void setup() {
 		memoryCard = Mockito.mock(MemoryCard.class);
@@ -20,26 +33,44 @@ public class CameraTest {
 		camera = new Camera(memoryCard, sensor);
 	}
 
+	/**
+     * Test Case 
+     * Switching the camera on powers up the sensor.
+     */
 	@Test
 	public void switchingTheCameraOnPowersUpTheSensor() {
 		camera.powerOn();
 		Mockito.verify(sensor).powerUp();
 	}
 
+	 /**
+     * Test Case
+     * Switching the camera off powers down the sensor
+     */
 	@Test
 	public void switchingTheCameraOffPowersDownTheSensor() {
 		camera.powerOff();
 		Mockito.verify(sensor).powerDown();
 	}
 
+	 /**
+     * Test Case
+     * Pressing the shutter when the power is off does nothing
+     */
 	@Test
 	public void perssingTheShutterWhenPowerOff() {
 		camera.powerOff();
 		camera.pressShutter();
+		
 		Mockito.verify(sensor).powerDown();
-		Mockito.verify(sensor, Mockito.never()).readData();
+		Mockito.verifyNoMoreInteractions(sensor, memoryCard);
 	}
 
+	/**
+     * Test Case
+     * Pressing the shutter when the power is on copies data from
+     * the sensor to the memory card.
+     */
 	@Test
 	public void perssingTheShutterWithPowerOn() {
 		camera.powerOn();
@@ -49,6 +80,11 @@ public class CameraTest {
 		Mockito.verify(memoryCard).write((Mockito.any(byte[].class)));
 	}
 
+	/**
+     * Test Case
+     * If data is currently being written, switching the camera 
+     * off does not power down the sensor.
+     */
 	@Test
 	public void switchingCameraOffWhenWriting() {
 		camera.powerOn();
@@ -58,6 +94,11 @@ public class CameraTest {
 		Mockito.verify(sensor, Mockito.never()).powerDown();
 	}
 
+	/**
+     * Test Case
+     * Once writing the data has completed, then the camera 
+     * powers down the sensor.
+     */
 	@Test
 	public void powerDownSensorOnceWritingCompleted() {
 		camera.powerOn();
